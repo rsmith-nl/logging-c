4d684c9a9fff327a85faafead142093540429d8e
Roland Smith <rsmith@xs4all.nl>
2026-02-18 23:44:54 +0100

    Start logging library.

---
+++ logging.c
@@ -0,0 +1,109 @@
+// file: logging.c
+// vim:fileencoding=utf-8:ft=c:tabstop=2
+//
+// Copyright © 2026 R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: MIT
+// Created: 2024-08-31 23:26:12 +0200
+// Last modified: 2026-02-18T23:40:46+0100
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include "logging.h"
+
+static char log_name[256] = {0};
+static char *prefix = "#%s %s: ";
+static int log_level = LOG_WARNING;
+
+void logging_configure(char *name, int level)
+{
+  assert(level>=0);
+  if (name) {
+    memcpy(log_name, name, 255);
+  }
+  if (level>0) {
+    log_level = level;
+  }
+}
+
+void debug(char *fmt, ...)
+{
+  if (log_level < LOG_DEBUG) {
+    return;
+  }
+  va_list args = {0};
+  assert(fmt);
+  fprintf(stderr, prefix, log_name, "DEBUG");
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  fputs("\n", stderr);
+  fflush(stderr);
+  va_end(args);
+}
+
+void info(char *fmt, ...)
+{
+  if (log_level < LOG_INFO) {
+    return;
+  }
+  va_list args = {0};
+  assert(fmt);
+  fprintf(stderr, prefix, log_name, "INFO");
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  fputs("\n", stderr);
+  fflush(stderr);
+  va_end(args);
+}
+
+void warning(char *fmt, ...)
+{
+  if (log_level < LOG_WARNING) {
+    return;
+  }
+  va_list args = {0};
+  assert(fmt);
+  fprintf(stderr, prefix, log_name, "WARNING");
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  fputs("\n", stderr);
+  fflush(stderr);
+  va_end(args);
+}
+
+void error(char *fmt, ...)
+{
+  if (log_level < LOG_ERROR) {
+    return;
+  }
+  va_list args = {0};
+  assert(fmt);
+  fprintf(stderr, prefix, log_name, "ERROR");
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  fputs("\n", stderr);
+  fflush(stderr);
+  va_end(args);
+}
+
+void critical(char *fmt, ...)
+{
+  if (log_level < LOG_CRITICAL) {
+    return;
+  }
+  va_list args = {0};
+  assert(fmt);
+  fprintf(stderr, prefix, log_name, "CRITICAL");
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  fputs("\n", stderr);
+  fflush(stderr);
+  va_end(args);
+  abort();
+}
---
+++ logging.h
@@ -0,0 +1,48 @@
+// file: logging.h
+// vim:fileencoding=utf-8:ft=c:tabstop=2
+//
+// Copyright © 2026 R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: MIT
+// Created: 2024-08-31 23:25:54 +0200
+// Last modified: 2026-02-18T23:36:35+0100
+
+#pragma once
+
+enum {
+  LOG_NOTSET = 0,
+  LOG_CRITICAL = 10,
+  LOG_ERROR = 20,
+  LOG_WARNING = 30,
+  LOG_INFO = 40,
+  LOG_DEBUG = 50
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Call this function before any of the other logging calls below.
+// The “name” is prepended to each log message.
+// Every mess with a “level” equal or higher than the given value is printed.
+// If “level” is 0, the logging level is set to LOG_WARNING.
+extern void logging_configure(char *name, int level);
+
+// Debug message. For developer infomation.
+extern void debug(char *fmt, ...);
+
+// Informational message.
+extern void info(char *fmt, ...);
+
+// An indication that something unexpected happened.
+extern void warning(char *fmt, ...);
+
+// Due to a more serious problem, the software has not been able to perform some function.
+extern void error(char *fmt, ...);
+
+// A problem so serious that the program cannot continue and will be aborted.
+extern void critical(char *fmt, ...);
+
+
+#ifdef __cplusplus
+}
+#endif
